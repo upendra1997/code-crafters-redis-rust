972cd0edbaed2760646c4291fd3361eb77495a57
Upendra Upadhyay <upendra.upadhyay@gojek.com>
2023-05-15 09:13:00 +0530

    using tokio for event loop instead of threads

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,43 @@
 // Uncomment this block to pass the first stage
 use std::{
     io::{Read, Write},
-    net::TcpListener,
     thread,
+};
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
 };
 
 use redis_starter_rust::handle_input;
 
 const REQUEST_BUFFER_SIZE: usize = 536870912;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+#[tokio::main]
+async fn main() {
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    thread::scope(|s| {
-        let mut handlers = vec![];
-        for stream in listener.incoming() {
-            match stream {
-                Ok(mut stream) => {
-                    let h = s.spawn(move || {
-                        println!("accepted new connection");
-                        let mut request_buffer = vec![0u8; REQUEST_BUFFER_SIZE];
-                        while let Ok(n) = stream.read(&mut request_buffer) {
-                            if n == 0 {
-                                eprintln!("read {} bytes", n);
-                                break;
-                            }
-                            println!("read {} bytes", n);
-                            let response = handle_input(&request_buffer[..n]);
-                            stream.write(&response);
-                            stream.flush();
-                        }
-                    });
-                    handlers.push(h);
-                }
-                Err(e) => {
-                    println!("error: {}", e);
+    loop {
+        match listener.accept().await {
+            Ok((mut stream, addr)) => {
+                println!("accepted new connection");
+                let mut request_buffer = vec![0u8; REQUEST_BUFFER_SIZE];
+                while let Ok(n) = stream.read(&mut request_buffer).await {
+                    if n == 0 {
+                        eprintln!("read {} bytes", n);
+                        break;
+                    }
+                    println!("read {} bytes", n);
+                    let response = handle_input(&request_buffer[..n]);
+                    stream.try_write(&response);
+                    stream.flush();
                 }
             }
+            Err(e) => {
+                eprintln!("error: {:?}", e);
+            }
         }
-    });
+    }
 }
