94ac0dde32029f97a55ee760a567d6391e2d130a
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2023-06-03 21:23:38 +0530

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
-use std::collections::VecDeque;
+use crate::resp::{Resp, SerDe};
+use std::{borrow::Cow, collections::VecDeque};
 
 mod resp;
 
 pub fn handle_input(request_buffer: &[u8]) -> Vec<u8> {
-    let (input, _) = resp::SerDe::deserialize(request_buffer);
+    let (input, _) = SerDe::deserialize(request_buffer);
     match input {
-        resp::Resp::Array(vec) => {
+        Resp::Array(vec) => {
             let mut arguments = VecDeque::from(vec);
             let command = arguments.pop_front().unwrap();
-            if let resp::Resp::Binary(command) = &command {
+            if let Resp::Binary(command) = &command {
                 let command = std::str::from_utf8(command).unwrap().to_uppercase();
                 match command.as_ref() {
-                    "PING" => resp::SerDe::serialize(resp::Resp::String("PONG".into())),
+                    "PING" => SerDe::serialize(Resp::String("PONG".into())),
                     "COMMAND" => {
                         let commands = vec![
                             "PING".as_bytes().into(),
@@ -19,25 +20,28 @@ pub fn handle_input(request_buffer: &[u8]) -> Vec<u8> {
                             "ECHO".as_bytes().into(),
                             vec!["response with some message".as_bytes().into()].into(),
                         ];
-                        resp::SerDe::serialize(Into::<resp::Resp>::into(commands))
+                        SerDe::serialize(Into::<Resp>::into(commands))
                     }
                     "ECHO" => {
                         let first = arguments.pop_front().unwrap();
-                        resp::SerDe::serialize(first)
+                        SerDe::serialize(first)
                     }
                     _ => {
                         eprintln!("invalid command {:?}", command);
-                        vec![]
+                        SerDe::serialize(Resp::Error(Cow::Owned(format!(
+                            "invalid command {:?}",
+                            command
+                        ))))
                     }
                 }
             } else {
                 vec![]
             }
         }
-        resp::Resp::Binary(_) => todo!(),
-        resp::Resp::Error(_) => todo!(),
-        resp::Resp::Integer(_) => todo!(),
-        resp::Resp::String(_) => todo!(),
-        resp::Resp::Null => todo!(),
+        Resp::Binary(_) => todo!(),
+        Resp::Error(_) => todo!(),
+        Resp::Integer(_) => todo!(),
+        Resp::String(_) => todo!(),
+        Resp::Null => todo!(),
     }
 }
