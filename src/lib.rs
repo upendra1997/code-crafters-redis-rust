4588122ed8448693695924c86a817fec54005a84
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2023-06-04 12:49:00 +0530

    added support expiring keys

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,20 @@ use crate::resp::{Resp, SerDe};
 use lazy_static::lazy_static;
 use std::{
     borrow::Cow,
-    collections::{HashMap, VecDeque},
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap, VecDeque},
+    str,
     sync::Mutex,
+    time::Duration,
 };
+use tokio::time::Instant;
 
 mod resp;
 
 lazy_static! {
     static ref STORE: Mutex<HashMap<Vec<u8>, Vec<u8>>> = Mutex::new(HashMap::new());
+    static ref EXPIRY: Mutex<BinaryHeap<(Reverse<Instant>, Vec<u8>)>> =
+        Mutex::new(BinaryHeap::new());
 }
 
 fn make_error(str: &str) -> Resp {
@@ -18,7 +24,7 @@ fn make_error(str: &str) -> Resp {
 
 fn handle_command(command: impl AsRef<[u8]>, mut arguments: VecDeque<Resp>) -> Vec<u8> {
     let command = command.as_ref();
-    let command = std::str::from_utf8(command);
+    let command = str::from_utf8(command);
     if let Err(_e) = command {
         return SerDe::serialize(make_error("command is not valid utf8"));
     }
@@ -57,7 +63,36 @@ fn handle_command(command: impl AsRef<[u8]>, mut arguments: VecDeque<Resp>) -> V
                 return SerDe::serialize(make_error("SET must be provided with a <value>"));
             }
             let second = second.unwrap();
-            STORE.lock().unwrap().insert(first.into(), second.into());
+            let key: Vec<u8> = first.into();
+            let value = second.into();
+            STORE.lock().unwrap().insert(key.clone(), value);
+            let third = arguments.pop_front();
+            if let Some(Resp::Binary(px)) = third {
+                if px.to_ascii_uppercase() != b"PX" {
+                    return SerDe::serialize(make_error(
+                        "error `SET <key> <value> PX <EXPIRY>` expected `PX`",
+                    ));
+                }
+                let fourth = arguments.pop_front();
+                if fourth.is_none() {
+                    return SerDe::serialize(make_error(
+                        "SET must be provided with a <EXPIRY> time",
+                    ));
+                }
+                let time = fourth.unwrap();
+                if let Resp::Binary(time) = time {
+                    if let Ok(Ok(time)) =
+                        str::from_utf8(time.as_ref()).map(|time| time.parse::<u64>())
+                    {
+                        let expiry = Instant::now()
+                            .checked_add(Duration::from_millis(time))
+                            .unwrap();
+                        EXPIRY.lock().unwrap().push((Reverse(expiry), key));
+                    } else {
+                        return SerDe::serialize(make_error("expiry time is not in range"));
+                    }
+                }
+            }
             SerDe::serialize(Resp::String(Cow::Borrowed("OK")))
         }
         "GET" => {
@@ -65,12 +100,25 @@ fn handle_command(command: impl AsRef<[u8]>, mut arguments: VecDeque<Resp>) -> V
             if first.is_none() {
                 return SerDe::serialize(make_error("GET must be provided with a <key>"));
             }
-            let first = first.unwrap();
-            let result = STORE.lock().unwrap().get::<Vec<u8>>(&first.into()).cloned();
+            let key = first.unwrap().into();
+            let mut store = STORE.lock().unwrap();
+            {
+                let mut expiry = EXPIRY.lock().unwrap();
+                while let Some(expire_key) = expiry.peek().map(|(time, _)| time.0 <= Instant::now())
+                {
+                    if expire_key {
+                        let (_, key) = expiry.pop().unwrap();
+                        store.remove(&key);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            let result = store.get::<Vec<u8>>(&key).cloned();
             if let Some(result) = result {
                 result
             } else {
-                SerDe::serialize(Resp::Error(Cow::Borrowed("Key not found")))
+                SerDe::serialize(Resp::Null)
             }
         }
         _ => {
