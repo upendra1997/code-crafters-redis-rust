9a843042a11a8f2efe71a9bf9c5e1ee6772b54f5
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2024-04-14 18:33:18 +0530

    enabling release mode

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -7,6 +7,7 @@
 # DON'T EDIT THIS!
 RUST_BACKTRACE=full exec cargo run \
     --quiet \
+    --release \
     --target-dir=/tmp/codecrafters-redis-target \
     --manifest-path $(dirname $0)/Cargo.toml \
     -- "$@"
