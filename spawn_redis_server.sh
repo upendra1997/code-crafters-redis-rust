2d25c0f954aca2c46e59b6822db8c210c3532c56
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2024-04-14 18:29:58 +0530

    removing release build from the codecrafter pipeline

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -7,7 +7,6 @@
 # DON'T EDIT THIS!
 RUST_BACKTRACE=full exec cargo run \
     --quiet \
-    --release \
     --target-dir=/tmp/codecrafters-redis-target \
     --manifest-path $(dirname $0)/Cargo.toml \
     -- "$@"
