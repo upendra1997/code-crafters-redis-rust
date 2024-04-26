3b43987da09bf0c3292b696eeed2628681c30d9d
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2024-04-27 01:01:33 +0530

    reducing the read and write timeout to 500 micros

--- src/main.rs
+++ src/main.rs
@@ -175,10 +175,10 @@ async fn main() {
                             let max = streams.keys().into_iter().max().map(|k| *k).unwrap_or(0);
                             let stream = stream.into_std().unwrap();
                             stream
-                                .set_write_timeout(Some(Duration::from_millis(1)))
+                                .set_write_timeout(Some(Duration::from_micros(500)))
                                 .unwrap();
                             stream
-                                .set_read_timeout(Some(Duration::from_millis(1)))
+                                .set_read_timeout(Some(Duration::from_micros(500)))
                                 .unwrap();
                             streams.insert(max + 1, (stream, 0));
                         }
