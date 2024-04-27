5552f2cae44a9daef1bd84cccf58dfe2797f6f7c
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2024-04-28 01:23:06 +0530

    increasing timeout

--- src/main.rs
+++ src/main.rs
@@ -115,7 +115,7 @@ async fn main() {
                             stream.try_write(&replconf_get_ack).unwrap();
                             stream.flush().await;
                             let mut request_buffer = vec![0u8; REQUEST_BUFFER_SIZE];
-                            timeout(Duration::from_millis(5), stream.readable()).await;
+                            timeout(Duration::from_millis(10), stream.readable()).await;
                             let res = stream.try_read(&mut request_buffer);
                             match res {
                                 Ok(0) => {
