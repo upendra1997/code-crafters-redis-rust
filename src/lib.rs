73d2c09fdd9b3f7fed362400f18ad4649472047b
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2024-04-27 20:30:34 +0530

    resetting offset to 0

--- src/lib.rs
+++ src/lib.rs
@@ -243,7 +243,7 @@ fn handle_command(
                             .parse::<u64>()
                             .unwrap();
                         // let old_replicas = NODE.write().unwrap().replicas.load(Ordering::SeqCst);
-                        // NODE.write().unwrap().replicas.store(0, Ordering::SeqCst);
+                        NODE.write().unwrap().replicas.store(0, Ordering::SeqCst);
                         NODE.read()
                             .unwrap()
                             .data_sender
